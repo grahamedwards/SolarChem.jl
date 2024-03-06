ffeb4f35c53f57ec497657fe6b6442d637556245
Graham H. Edwards <grahamhedwards92@gmail.com>
2024-03-06 13:51:56 -0500

    remove x86 architecture from CI test

--- src/SolarChem.jl
+++ src/SolarChem.jl
@@ -17,8 +17,8 @@ export calcweights, estimateuncertainty, trimnans, trimextremes, pulltopic, pull
 include("data-mgmt.jl")
 
 function __init__()
-    Requires.@require CairoMakie="13f3f980-e62b-5c42-98c6-ff1f3baf88f0" include("visualization.jl") 
-    #GLMakie = "e9467ef8-e4e7-5192-8a1a-b1aee30e663a"
+    Requires.@require CairoMakie="13f3f980-e62b-5c42-98c6-ff1f3baf88f0" include("visualization.jl")
+    Requires.@require GLMakie="e9467ef8-e4e7-5192-8a1a-b1aee30e663a" include("visualization.jl") 
 end
 
 end
