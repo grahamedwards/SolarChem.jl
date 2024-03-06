e06f833e4abb7a4734936e89b8f6d0b4f5cf593b
Graham H. Edwards <grahamhedwards92@gmail.com>
2024-03-06 13:50:46 -0500

    set up visualization stuff

--- src/SolarChem.jl
+++ src/SolarChem.jl
@@ -1,6 +1,6 @@
 module SolarChem
 
-import Random, DelimitedFiles
+import Random, DelimitedFiles, Requires
 using VectorizedStatistics: vmean, vmedian
 using NaNStatistics: nanmean, nanmedian, countnotnans
 
@@ -15,5 +15,10 @@ include("resamplers.jl")
 
 export calcweights, estimateuncertainty, trimnans, trimextremes, pulltopic, pulltype, pullgroup, exclude, excludeheated, countratios, countmeasurements
 include("data-mgmt.jl")
+
+function __init__()
+    Requires.@require CairoMakie="13f3f980-e62b-5c42-98c6-ff1f3baf88f0" include("visualization.jl") 
+    #GLMakie = "e9467ef8-e4e7-5192-8a1a-b1aee30e663a"
+end
 
 end
